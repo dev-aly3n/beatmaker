cebcf779ecf3c30b3220fc33c217a4fcbc8aeee3
aly3n <aly3n.dev@gmail.com>
2021-04-19 20:50:49 +0430

    adding some functionality to the app

--- build/app.js
+++ build/app.js
@@ -1,36 +1,92 @@
 // modern Navigation bar code start here
 const navSlide = () => {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
-    //toggle nav
-    burger.addEventListener('click', () => {
-        console.log(nav.classList.contains('nav-active'))
-        nav.classList.toggle('nav-active');
+  //toggle nav
+  burger.addEventListener("click", () => {
+    console.log(nav.classList.contains("nav-active"));
+    nav.classList.toggle("nav-active");
 
-        //animate links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.6}s`;
-            }
-        });
-
-        //burger animation 
-        burger.classList.toggle('toggle');
+    //animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.6
+        }s`;
+      }
     });
 
-    document.addEventListener("click", e => {
-        if (e.target.matches('.navigation')) return
-        nav.classList.remove('nav-active')
-        burger.classList.remove('toggle');
-        navLinks.forEach((link) => {
-            link.style.animation = '';
-        });
+    //burger animation
+    burger.classList.toggle("toggle");
+  });
+
+  document.addEventListener("click", (e) => {
+    if (e.target.matches(".navigation")) return;
+    nav.classList.remove("nav-active");
+    burger.classList.remove("toggle");
+    navLinks.forEach((link) => {
+      link.style.animation = "";
     });
-}
+  });
+};
 navSlide();
 // modern Navigation bar code stop here
 
+//drum app start here
+
+class Drum {
+  constructor() {
+    this.pads = document.querySelectorAll(".pad");
+    this.kickAudio = document.querySelector(".kick-sound");
+    this.snareAudio = document.querySelector(".snare-sound");
+    this.hihatAudio = document.querySelector(".hihat-sound");
+    this.clapAudio = document.querySelector(".clap-sound");
+    this.effectAudio = document.querySelector(".effect-sound");
+    this.playBtn = document.querySelector(".play");
+    this.index = 0;
+    this.bpm = 100;
+  }
+
+  // repeating the pads in a loop.  reminder of 10 will make the step start from 0 to 9
+  //and active bars will show the column of bars that they currently active
+  // this keyword reffer to this object here
+  repeat() {
+    let step = this.index % 10;
+    const activeBars = document.querySelectorAll(`b${step}`);
+    this.index++;
+  }
+
+  // start the loop by an interval. bpm will handle the speed of the loop
+  // the arrow function here allow us to use this keyword reffering to the current object
+  // this.index will reffer to the times of the loop running. so by deviding to 10 the reminder will looping between 0 - 9
+  start() {
+    const interval = (60 / this.bpm) * 1000;
+    setInterval(() => {
+      this.repeat();
+    }, interval);
+  }
+
+  // toggle the class active to the pads after clicking on them
+  activePad() {
+    this.classList.toggle("active");
+  }
+}
+
+// making a new object of the class Drum
+const drum = new Drum();
+
+//add event listener to every pad that had been clicked and make them active
+drum.pads.forEach((pad) => {
+  pad.addEventListener("click", drum.activePad);
+});
+
+// start the loop by clicking on the play button
+// we use a normal function BCS the if we just use drum.start here, the this keyword will reffer to the button in the ...
+// ... start() method
+drum.playBtn.addEventListener("click", function () {
+  drum.start();
+});
