f87af22e8f5ab5f279e18078deeb7ef9e821e8c2
aly3n <aly3n.dev@gmail.com>
2021-04-24 23:29:09 +0430

    randomize btn added

--- build/app.js
+++ build/app.js
@@ -51,13 +51,14 @@ class Drum {
     this.currentHihat = "./sounds/hihat4.mp3";
     this.currentClap = "./sounds/clap1.mp3";
     this.currentEffect = "./sounds/effect7.mp3";
-    this.selectS = document.querySelectorAll('select');
+    this.selectS = document.querySelectorAll("select");
     this.playBtn = document.querySelector(".play");
     this.index = 0;
     this.bpm = 200;
     this.play = null;
-    this.muteBtnS = document.querySelectorAll('.mute');
-    this.tempoSlider = document.querySelector('.tempo-slider');
+    this.muteBtnS = document.querySelectorAll(".mute");
+    this.tempoSlider = document.querySelector(".tempo-slider");
+    this.randomizeBtn = document.querySelectorAll('.randomize');
   }
 
   // repeating the pads in a loop.  reminder of 10 will make the step start from 0 to 9
@@ -68,30 +69,29 @@ class Drum {
     const activeBars = document.querySelectorAll(`.b${step}`);
 
     // appending an animation style to the current pads and then remove them after  finishing animation by an eventlistener to repeat again
-    
+
     activeBars.forEach((pad) => {
       pad.style.animation = `boomBoom 0.25s alternate ease-in-out 2`;
 
       // fixing the issue with repeating the song in every pad
       if (pad.classList.contains("active")) {
-
-        if (pad.classList.contains('kick-pad')) {
+        if (pad.classList.contains("kick-pad")) {
           this.kickAudio.currentTime = 0;
           this.kickAudio.play();
         }
-        if (pad.classList.contains('snare-pad')) {
+        if (pad.classList.contains("snare-pad")) {
           this.snareAudio.currentTime = 0;
           this.snareAudio.play();
         }
-        if (pad.classList.contains('hihat-pad')) {
+        if (pad.classList.contains("hihat-pad")) {
           this.hihatAudio.currentTime = 0;
           this.hihatAudio.play();
         }
-        if (pad.classList.contains('clap-pad')) {
+        if (pad.classList.contains("clap-pad")) {
           this.clapAudio.currentTime = 0;
           this.clapAudio.play();
         }
-        if (pad.classList.contains('effect-pad')) {
+        if (pad.classList.contains("effect-pad")) {
           this.effectAudio.currentTime = 0;
           this.effectAudio.play();
         }
@@ -99,7 +99,6 @@ class Drum {
     });
     this.index++;
   }
-
 
   // start the loop by an interval. bpm will handle the speed of the loop
   // the arrow function here allow us to use this keyword reffering to the current object
@@ -111,16 +110,15 @@ class Drum {
     //adding class active that can be usefull in when we want to check if the tracks are playing or not
     if (!this.play) {
       this.playBtn.innerText = "Stop";
-      this.playBtn.classList.add('active');
+      this.playBtn.classList.add("active");
       this.play = setInterval(() => {
         this.repeat();
       }, interval);
     } else {
       this.playBtn.innerText = "Play";
-      this.playBtn.classList.remove('active');
-      clearInterval(this.play)
+      this.playBtn.classList.remove("active");
+      clearInterval(this.play);
       this.play = null;
-
     }
   }
 
@@ -156,11 +154,10 @@ class Drum {
   // muting a song when it isn't mute and unmuting them
   //and add toggle the class active to the mute btn
   mute(e) {
-    const muteName = e.target.getAttribute('data-track');
+    const muteName = e.target.getAttribute("data-track");
     const muteClass = e.target.classList;
-    muteClass.toggle('active');
-    if (muteClass.contains('active')) {
-
+    muteClass.toggle("active");
+    if (muteClass.contains("active")) {
       switch (muteName) {
         case "kick":
           this.kickAudio.volume = 0;
@@ -195,27 +192,50 @@ class Drum {
         case "effect":
           this.effectAudio.volume = 1;
           break;
+      }
     }
+  }
+
+  //change the bpm of the track by a slider
+  //change the text parameter of the tempo in the same time
+  changeTempo(e) {
+    document.querySelector(".tempo-num").innerText = e.target.value;
+  }
+  // change the actual bpm after that user changed the slider
+  // we reset all the parameter and then check if the song is now playing? if yes then we start it with new bpm and else we do nothing
+  updateTempo(e) {
+    this.bpm = e.target.value;
+    clearInterval(this.play);
+    this.play = null;
+    const playBtn = document.querySelector(".play");
+    if (playBtn.classList.contains("active")) {
+      this.start();
+    }
+  }
+
+  randomizer(e){
+    const randomName = e.target.getAttribute("data-track");
+    const randomPad = document.querySelectorAll(`.${randomName}-pad`);
+    randomPad.forEach(pad => pad.classList.remove('active'));
+    randomPad.forEach(el => {
+      if (this.random()){
+        el.classList.add('active');
+      }
+    });
+
+  }
+
+  random(){
+return (Math.random() < 0.5);
   }
 }
 
-//change the bpm of the track by a slider
-//change the text parameter of the tempo in the same time
-changeTempo(e){
-  document.querySelector('.tempo-num').innerText = e.target.value;
-}
-// change the actual bpm after that user changed the slider
-// we reset all the parameter and then check if the song is now playing? if yes then we start it with new bpm and else we do nothing
-updateTempo(e){
-  this.bpm = e.target.value;
-  clearInterval(this.play);
-  this.play = null;
-  const playBtn = document.querySelector('.play');
-  if(playBtn.classList.contains('active')){
-    this.start();
-  }
-}
-}
+
+///////////////////////////
+//////////////////////
+/////////////////
+
+
 
 // making a new object of the class Drum
 const drum = new Drum();
@@ -223,9 +243,9 @@ const drum = new Drum();
 //add event listener to every pad that had been clicked and make them active
 drum.pads.forEach((pad) => {
   pad.addEventListener("click", drum.activePad);
-  pad.addEventListener('animationend', function () {
-    this.style.animation = '';
-  })
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
 });
 
 // start the loop by clicking on the play button
@@ -236,25 +256,31 @@ drum.playBtn.addEventListener("click", function () {
 });
 
 //changing the sounds when the user change it trough the select options
-drum.selectS.forEach(select => {
-  select.addEventListener('change', function (e) {
+drum.selectS.forEach((select) => {
+  select.addEventListener("change", function (e) {
     drum.changeSound(e);
   });
 });
 
 //muting the track when user clicked on the mute btn
-drum.muteBtnS.forEach(btn => {
-  btn.addEventListener('click', function (e) {
+drum.muteBtnS.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
     drum.mute(e);
   });
 });
 
 //changing the text of tempo slider in every moment
-drum.tempoSlider.addEventListener('input', function(e){
+drum.tempoSlider.addEventListener("input", function (e) {
   drum.changeTempo(e);
 });
 
 //changing the bpm when the user changed the tempo slider
-drum.tempoSlider.addEventListener('change', function(e){
+drum.tempoSlider.addEventListener("change", function (e) {
   drum.updateTempo(e);
+});
+
+drum.randomizeBtn.forEach(btn => {
+btn.addEventListener('click', function(e){
+  drum.randomizer(e);
+})
 });
